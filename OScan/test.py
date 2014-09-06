383f5a26f7807d9f448f1689963af47ee1cf5776
Chris <theflexmaster@gmail.com>
2014-09-06 14:20:27 -0400

    Removed danica comment. She stinks.

--- OScan/test.py
+++ OScan/test.py
@@ -1,14 +1,12 @@
 import os
 import sys
 
-#Danica is cool.
 DIR = os.path.join(os.path.dirname(__file__), '..')
 sys.path.insert(0, DIR)
 
 from OScan import ui
 reload(ui)
 from PySide import QtGui
-
 
 if __name__ == '__main__':
     qtapp = QtGui.QApplication(sys.argv)
