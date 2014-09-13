d380e359d18fbfe808f04f35f16420374b5bfad2
Chris <theflexmaster@gmail.com>
2014-09-13 16:17:00 -0400

    Added git ignore and added decimal precision to calculator.

--- OScan/calculator.py
+++ OScan/calculator.py
@@ -1,6 +1,6 @@
+from decimal import Decimal
 
-
-def calcOverscan(oScan=1.1,
+def calculateOverscan(oScan=1.1,
                  xRes=2048,
                  yRes=1080,
                  xAper=0.9449244,
@@ -29,35 +29,45 @@ def calcOverscan(oScan=1.1,
     Raises:
         ValueError: If the oScan is equal to or less than 1.0.
     '''
-
     if oScan <= 1.000:
         raise ValueError("Overscan value is equal to or less than 1.0")
 
-    oXres = xRes * oScan
-    oYres = yRes * oScan
+    #Coverting values for decimal.
+    dec_OScan = Decimal(str(oScan))
+    dec_xRes = Decimal(str(xRes))
+    dec_yRes = Decimal(str(yRes))
 
-    rX = round(oXres)
-    rY = round(oYres)
+    #Calculate overscan res.
+    oXres = dec_xRes * dec_OScan
+    oYres = dec_yRes * dec_OScan
 
-    if rX % 2 == 1:
-        rX += 1.0
+    #Round to int.
+    resX = round(oXres)
+    resY = round(oYres)
 
-    if rY % 2 == 1:
-        rY += 1.0
+    #Make sure it's even on X.
+    if resX % 2 == 1:
+        resX += 1.0
 
-    xScale = rX/xRes
-    yScale = rY/yRes
+    #Make sure it's even on Y.
+    if resY % 2 == 1:
+        resY += 1.0
 
-    oXAper = xAper * xScale
-    oYAper = yAper * yScale
+    #Find x and y scale values.
+    xScale = Decimal(str(resX))/dec_xRes
+    yScale = Decimal(str(resY))/dec_yRes
 
-    return [int(rX), int(rY)], [oXAper, oYAper]
+    #Calculate aperture.
+    oXAper = Decimal(str(xAper)) * Decimal(str(xScale))
+    oYAper = Decimal(str(yAper)) * Decimal(str(yScale))
+
+    return [int(resX), int(resY)], [float(oXAper), float(oYAper)]
 
 if __name__ == '__main__':
-    res, aper = calcOverscan(oScan=1.1,
-                             xRes=2048,
-                             yRes=1080,
-                             xAper=0.9449244,
-                             yAper=0.4982999)
+    res, aper = calculateOverscan(oScan=1.1,
+                                  xRes=2048,
+                                  yRes=1080,
+                                  xAper=0.9449244,
+                                  yAper=0.4982999)
 
     print(res, aper)
